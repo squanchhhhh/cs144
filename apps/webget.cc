a31e97ecf5931d92869b4be12b03d8ddb9145658
squanch <a246004219@gmail.com>
2024-06-29 22:17:47 +0800

    webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,16 +1,45 @@
-#include "socket.hh"
-
+#include "../util/socket.hh"
+#include "../util/address.hh"
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <span>
 #include <string>
+#include <sys/socket.h>
 
 using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  char request[256]; // Assuming a reasonable buffer size for the request
+  std::sprintf(request, "GET http://%s%s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", host.c_str(), path.c_str(), host.c_str());
+  auto client = TCPSocket();
+  // auto address = Address(host,80);
+  // Define the proxy settings
+  const string proxy_host = "192.168.0.100"; 
+  const string proxy_port = "7890"; 
+  addrinfo hints;
+  std::memset(&hints, 0, sizeof(addrinfo));
+  hints.ai_family = AF_UNSPEC;    // Allow IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+    // Connect to the proxy server
+  auto proxy_address = Address(proxy_host, proxy_port);
+  client.connect(proxy_address);
+
+  client.write(request);
+  cout<<request;
+  client.shutdown(SHUT_WR);
+  // Read the response from the proxy server
+  string response;
+  while (!client.eof()) {
+      string temp;
+      client.read(temp);
+      response += temp;
+  }
+  cout<<response<<endl;
+  client.close();
+  // cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+  // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main( int argc, char* argv[] )
